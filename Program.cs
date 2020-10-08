9097946996693aab0237e00ec51ccfd8f88fc970
Geethu Joseph <Gjoseph1314@conestogac.on.ca>
2020-10-08 22:43:26 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Triangle
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string cont;
+            string userInput = string.Empty;
+            bool Validmenuselect = false;
+            string resultString = string.Empty;
+
+
+            while (Validmenuselect == false)
+            {
+                do
+                {
+                    Console.WriteLine("1.Enter the triangle dimensions");
+                    Console.WriteLine("2.Exit");
+                    Console.WriteLine("Please select an option by entering a valid number from the ablove list");
+                    userInput = Console.ReadLine();
+                    if (userInput != "1" && userInput != "2")
+                    {
+                        Console.WriteLine("Please enter a Valid option");
+                    }
+                    else if (userInput == "2")
+                    {
+
+                        Console.WriteLine("Exiting from the program");
+                        System.Environment.Exit(0);
+                    }
+                    else
+                    {
+                        Validmenuselect = true;
+                        string side1, side2, side3;
+                        int sideint1, sideint2, sideint3;
+                        Console.WriteLine("Enter the first side");
+                        side1 = Console.ReadLine();
+                        Console.WriteLine("Enter the second side");
+                        side2 = Console.ReadLine();
+                        Console.WriteLine("Enter the third side");
+                        side3 = Console.ReadLine();
+                        if ((int.TryParse(side1, out sideint1)) && (int.TryParse(side2, out sideint2)) && (int.TryParse(side3, out sideint3)) && (sideint1 > 0) && (sideint2 > 0) && (sideint3 > 0))
+                        {
+
+                            Console.WriteLine(TriangleSolver.Analyze(sideint1, sideint2, sideint3));
+
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("The given inputs doesnot form a triangle!!");
+                        }
+
+
+                    }
+
+                    Console.WriteLine("Do you want to continue?:(Y/N)");
+                    cont = Console.ReadLine();
+                } while ((cont == "Y") || (cont == "y"));
+
+            }
+
+
+
+        }
+
+
+
+    }
+
+}
---
+++ TriangleSolver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Triangle
+{
+    public static class TriangleSolver
+    {
+        public static string Analyze(int sideint1, int sideint2, int sideint3)
+        {
+
+            string result = "The given inputs doesnot form a triangle!!";
+            if (sideint1 + sideint2 > sideint3 && sideint1 + sideint3 > sideint2 && sideint2 + sideint3 > sideint1)
+            {
+
+                if ((sideint1 == sideint2) && (sideint2 == sideint3))
+                {
+                    //result = result + "Equilateral Triangle";
+                    result = "Equilateral Triangle";
+                }
+                //a == b or b=c or a =c
+                else if ((sideint1 == sideint2) || (sideint2 == sideint3) || (sideint3 == sideint1))
+                {
+                    result = "Isoceles Triangle";
+                }
+                else if (sideint1 != sideint2 && sideint2 != sideint3)
+                {
+                    result = "scalene triangle";
+                }
+
+            }
+            return result;
+        }
+    }
+}
